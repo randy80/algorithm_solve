369817e100224308edd8a38b7bbd117662e166a6
InSeong Lee <randy80@gmail.com>
2022-12-28 18:17:31 +0900

    [코딩인터뷰 완전분석] 1.1 중복이 없는가

--- src/test/java/dev/randy80/data_structure/DuplicationTest.java
+++ src/test/java/dev/randy80/data_structure/DuplicationTest.java
@@ -1,5 +1,6 @@
 package dev.randy80.data_structure;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,12 +8,14 @@ import static org.hamcrest.Matchers.is;
 
 public class DuplicationTest {
     @Test
+    @DisplayName("중복이 없음")
     public void testNoDuplication() {
         Duplication duplication = new Duplication();
         assertThat(duplication.check("abcdefg"), is(false));
     }
 
     @Test
+    @DisplayName("중복이 있음")
     public void testIsDuplication() {
         Duplication duplication = new Duplication();
         assertThat(duplication.check("abcdeabfg"), is(true));
